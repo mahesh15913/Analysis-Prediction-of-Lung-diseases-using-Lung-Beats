fa8f5c585f0dd29d5979bccb4711fbcbc1215826
mahii <mahii.123@outlook.com>
2024-04-16 21:02:59 +0530

    files are created

---
+++ lungwebpage/front.js
@@ -0,0 +1,13 @@
+function aboutus(){
+    window.location.href = "aboutus.html"
+}
+
+function analysis(){
+    window.location.href = "analysis.html"
+}
+function prediction(){
+    window.location.href = "prediction.html"
+}
+function project(){
+    window.location.href = "project.html"
+}
