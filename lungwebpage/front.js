e4fadcb9db2bc0cb4a75153ce43840f5c733a5ae
mahii <mahii.123@outlook.com>
2024-05-09 16:42:34 +0530

    webpage title tab was modified

--- lungwebpage/front.js
+++ lungwebpage/front.js
@@ -14,38 +14,8 @@ function project(){
 function literature(){
     window.location.href = 'https://drive.google.com/file/d/1XKUObNXhzhPm3Y1MX3stm05Rbuek5zSv/view?usp=sharing'
 }
-function insta(x){
-    if(x==1){
-        window.location.href = "maheshvejandla5@gmail.com"
-    }
-    if(x==2){
-        window.location.href = "https://www.instagram.com/.mahesh._18.?utm_source=qr&igsh=dzVjczhvOW95OGs5"
-    }
-    if(x==3){
-        window.location.href = "maheshvejandla5@gmail.com"
-    }
-}
-
-function github(x){
-    if(x==1){
-        window.location.href = "https://github.com/Avneesh1811"
-    }
-    if(x==2){
-        window.location.href = "https://github.com/mahesh15913"
-    }
-    if(x==3){
-        window.location.href = "https://github.com/RACHITSHRI"
-    }
-}
-
-function linkdin(x){
-    if(x==1){
-        window.location.href = "https://www.linkedin.com/in/avneesh-tamra-5039b0212/"
-    }
-    if(x==2){
-        window.location.href = "https://www.linkedin.com/in/vejandla-mahesh-babu/"
-    }
-    if(x==3){
-        window.location.href = "https://www.linkedin.com/in/rachit-shrivastava-a17181252/"
-    }
-}
+const imageGallery = document.querySelector('.image-gallery');
+imageGallery.addEventListener('wheel', (event) => {
+    event.preventDefault();
+    imageGallery.scrollLeft += event.deltaY;
+});
